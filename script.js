cf27e4c9aa5e44e7f014f8f6e61dc9553efe32a2
Biatriz-Raiol <biateixeira524@gmail.com>
2025-11-13 15:44:11 -0300

    conseguir fazer o filtro funcionar com javascript

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const botoesFiltro = document.querySelectorAll(".filtro-btn");
+const cards = document.querySelectorAll(".card");
 
+botoesFiltro.forEach(botao => {
+    botao.addEventListener("click", () => {
+    botoesFiltro.forEach(btn => btn.classList.remove("ativo"));
+    botao.classList.add("ativo");
+
+    const categoria = botao.getAttribute("data-categoria");
+
+    cards.forEach(card => {
+        if (categoria === "todos" || card.getAttribute("data-categoria") === categoria) {
+        card.style.display = "flex";
+    } else {
+        card.style.display = "none";
+    }
+    });
+});
+});
