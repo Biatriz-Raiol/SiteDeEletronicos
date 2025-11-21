03b22d2ec54d9143d09c1c9cb0b4cbfceed31f7f
ViniRaiz7 <lendalv444@gmail.com>
2025-11-21 14:57:49 -0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,8 @@
+<?php
+?>
+<footer>
+    <div class="container">
+        <p>&copy; 2025 HawkTech Eletrônicos - Todos os direitos reservados</p>
+        <p>Desenvolvido por: Biatriz Raiol Teixeira, Letícia Aleixo Sampaio, Lucas Vinicius Lopes Viana, Maysa da Silva Leite Barros</p>
+    </div>
+</footer>
